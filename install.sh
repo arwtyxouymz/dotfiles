3055934528a4ac1187a9b24b5cb1997f4225745c
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2018-04-09 17:00:31 +0900

    changed command check

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ DOTPATH=~/dotfiles
 GITHUB_URL=https://github.com/arwtyxouymz/dotfiles
 
 # git が使えるなら git
-if [ has "git" ]; then
+if type git > /dev/null 2>&1; then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
-elif [ has "curl" || has "wget" ]; then
+elif type curl > /dev/null 2>&1 || type wget > /dev/null 2>&1; then
     tarball="https://github.com/arwtyxouymz/dotfiles/archive/master.tar.gz"
 
     # どっちかでダウンロードして，tar に流す
-    if [ has "curl" ]; then
+    if type curl > /dev/null 2>&1; then
         curl -L "$tarball"
 
-    elif [ has "wget" ]; then
+    elif type wget > /dev/null 2>&1; then
         wget -O - "$tarball"
     fi | tar xv -
 
