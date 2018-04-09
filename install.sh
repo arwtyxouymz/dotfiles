01e826cdffe331f258fa4bff21552c4798bf7aec
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2018-04-09 16:50:02 +0900

    fixed if structure

--- install.sh
+++ install.sh
@@ -6,27 +6,24 @@ DOTPATH=~/dotfiles
 GITHUB_URL=https://github.com/arwtyxouymz/dotfiles
 
 # git が使えるなら git
-if has "git"; then
+if [ has "git" ]; then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
-
-# 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
+elif [ has "curl" || has "wget" ]; then
     tarball="https://github.com/arwtyxouymz/dotfiles/archive/master.tar.gz"
 
     # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
+    if [ has "curl" ]; then
         curl -L "$tarball"
 
-    elif has "wget"; then
+    elif [ has "wget" ]; then
         wget -O - "$tarball"
-
     fi | tar xv -
 
     # 解凍したら，DOTPATH に置く
     mv -f dotfiles-master "$DOTPATH"
-
 else
-    die "curl or wget required"
+    echo "curl or wget required"
+    exit 1
 fi
 
 cd ~/.dotfiles
