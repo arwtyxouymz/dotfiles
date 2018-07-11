c8e4f40a06af5972f78b8043af0ffd9ffcdeb73e
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2018-07-11 16:48:16 +0900

    fixed phpbrew switch problem

--- bin/web.sh
+++ bin/web.sh
@@ -13,8 +13,13 @@ echo $PASSWORD | sudo -S mv phpbrew /usr/local/bin/phpbrew
 
 phpbrew init
 phpbrew self-update
+# phpのインストール
 phpbrew install ${PHP} +default +mysql +openssl=$(brew --prefix openssl)
+# phpbrewの設定読み込み
+source $HOME/.bash_profile
+# phpの切り替え
 phpbrew switch php-${PHP}
+# 拡張のインストール
 phpbrew ext install xdebug stable
 phpbrew ext install gd -- --with-jpeg-dir=$(brew --prefix jpeg)
 
