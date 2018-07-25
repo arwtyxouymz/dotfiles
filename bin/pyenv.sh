544e4705e55bd8d50abd4f8a5532baa2e76417c3
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2018-07-25 11:06:31 +0900

    changed flake8 install to pyenv-python

--- bin/pyenv.sh
+++ bin/pyenv.sh
@@ -7,11 +7,11 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 pyenv install ${PYTHON3}
 pip install --upgrade pip
+pip3 install flake8
 pyenv virtualenv ${PYTHON3} neovim3
 pyenv activate neovim3
 pip install --upgrade pip
 pip3 install neovim
-pip3 install flake8
 pip3 install git+https://github.com/dpallot/simple-websocket-server.git
 pip3 install python-slugify
 pip3 install notedown
@@ -19,11 +19,11 @@ pyenv deactivate
 
 pyenv install ${PYTHON2}
 pip install --upgrade pip
+pip2 install flake8
 pyenv virtualenv ${PYTHON2} neovim2
 pyenv activate neovim2
 pip install --upgrade pip
 pip2 install neovim
-pip2 install flake8
 pyenv deactivate
 
 pyenv virtualenv ${PYTHON3} Cpp
