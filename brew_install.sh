429339b930e7af66cbf05836ea1ca63c83cf00fb
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2020-04-18 11:57:18 +0900

    added brew install lists

---
+++ brew_install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+brew update && brew upgrade
+brew cask upgrade
+
+# shell
+brew install zsh
+brew install starship
+
+# GUI tools
+brew cask install alfred
+brew cask install iterm2
+brew cask install alt-tab
+brew cask install google-chrome
+
+# commands
+brew install bat
+brew install exa
+brew install hexyl
+brew install fd
+brew install ripgrep
+brew install procs
+brew tap cjbassi/ytop
+brew install ytop
+
+# languages
+brew install node
+brew install yarn
+brew install go
+brew install pyenv pyenv-virtualenv
+
+# Finally
+brew cleanup
