67dbbb715f61849744f2772ddb8bc128b412a63e
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2020-12-25 19:08:07 +0900

    checkpoint

---
+++ .config/nvim/template/markdown/base-meta.md
@@ -0,0 +1,7 @@
+---
+id: {{_name_}}
+title: 
+description: 
+postedAt: {{_expr_:strftime('%Y/%m/%d')}}
+tags: []
+---
---
+++ .config/nvim/template/typescriptreact/base-standard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const {{_name_}}: React.FC = () => {
+  return <div></div>
+}
+
+export default {{_name_}}
